99a44120d361af65b3b2aba91ee6996b6be6d3d1
Mani1607 <manisha.creativedesigner@gmail.com>
2023-09-19 17:46:57 +0400

    ARCore Project uploaded

---
+++ ARCore1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,65 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ARTapToPlace::Start()
+extern void ARTapToPlace_Start_mBC3BFCD2258CF2C5CD27BED287ABBD164616F59B (void);
+// 0x00000002 System.Void ARTapToPlace::Update()
+extern void ARTapToPlace_Update_m0FDBA559D7ABA80E790C317924E9B25FFD724D05 (void);
+// 0x00000003 System.Void ARTapToPlace::UpdatePlacementPose()
+extern void ARTapToPlace_UpdatePlacementPose_m1018B63D55B078806BE04869DAAF61535ACB0EE1 (void);
+// 0x00000004 System.Void ARTapToPlace::PlaceObject()
+extern void ARTapToPlace_PlaceObject_m3D02B3E7FA2619F217A0DBD1F65F9B4D9269D211 (void);
+// 0x00000005 System.Void ARTapToPlace::UpdatePlacementIndicator()
+extern void ARTapToPlace_UpdatePlacementIndicator_mC797F550F4840511870B2400D6BD0B53A4527F20 (void);
+// 0x00000006 System.Void ARTapToPlace::.ctor()
+extern void ARTapToPlace__ctor_mDD8E97029B4DDB0187EDA173CA4879148DE758CE (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	ARTapToPlace_Start_mBC3BFCD2258CF2C5CD27BED287ABBD164616F59B,
+	ARTapToPlace_Update_m0FDBA559D7ABA80E790C317924E9B25FFD724D05,
+	ARTapToPlace_UpdatePlacementPose_m1018B63D55B078806BE04869DAAF61535ACB0EE1,
+	ARTapToPlace_PlaceObject_m3D02B3E7FA2619F217A0DBD1F65F9B4D9269D211,
+	ARTapToPlace_UpdatePlacementIndicator_mC797F550F4840511870B2400D6BD0B53A4527F20,
+	ARTapToPlace__ctor_mDD8E97029B4DDB0187EDA173CA4879148DE758CE,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	4171,
+	4171,
+	4171,
+	4171,
+	4171,
+	4171,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
